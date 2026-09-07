3799ec8214aee2542343f8fbfb8dc25b64f0c88e
Sujeet Sahani <sujeetsahani874@gmail.com>
2026-09-07 20:46:33 +0530

    Allow production frontend in CORS

--- server/index.js
+++ server/index.js
@@ -10,9 +10,20 @@ import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://interview-iq-sujeet13.vercel.app"
+]
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true
 }))
 
 app.use(express.json())
