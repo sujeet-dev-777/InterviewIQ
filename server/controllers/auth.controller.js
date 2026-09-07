4fd4d9459c2040d76ad21aa80c723ec381f91dae
Sujeet Sahani <sujeetsahani874@gmail.com>
2026-09-07 21:05:20 +0530

    Fix production authentication cookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -15,8 +15,8 @@ export const googleAuth = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token" , token , {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -32,7 +32,11 @@ export const googleAuth = async (req,res) => {
 
 export const logOut = async (req,res) => {
     try {
-        await res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
         return res.status(200).json({message:"LogOut Successfully"})
     } catch (error) {
          return res.status(500).json({message:`Logout error ${error}`})
